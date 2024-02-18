d11dad410dbec6b92722c683c037afde0802d2ff
NHeroOP <neeravtheheroo@gmail.com>
2024-02-18 16:54:34 +0530

    Add constants

---
+++ constants.js
@@ -0,0 +1,44 @@
+const playListData = [
+  {
+    id: 1,
+    title: 'Shape Of You',
+    artist: 'Ed Sheeran',
+    album: '÷ (Deluxe)',
+    artwork: "https://i.scdn.co/image/ab67616d0000b273ba5db46f4b838ef6027e6f96",
+    url: './assets/audio/one.mp3',
+  },
+  {
+    id: 2,
+    title: 'Cheap Thrills',
+    artist: 'Sia',
+    album: 'This is Acting',
+    artwork: "https://i.scdn.co/image/ab67616d0000b27349e0134c686547c28b7c999f",
+    url: './assets/audio/two.mp3',
+  },
+  {
+    id: 3,
+    title: 'Happier',
+    artist: 'Marshmello, Bastile',
+    album: 'Single',
+    artwork: "https://i.scdn.co/image/ab67616d0000b273dd0a40eecd4b13e4c59988da",
+    url: './assets/audio/three.mp3',
+  },
+  {
+    id: 4,
+    title: 'Without Me',
+    artist: 'Halsey',
+    album: 'Single',
+    artwork: "https://i.scdn.co/image/ab67616d0000b2737636e1c9e67eaafc9f49aefd",
+    url: './assets/audio/four.mp3',
+  },
+  {
+    id: 5,
+    title: 'Memories',
+    artist: 'Maroon 5',
+    album: 'JORDI (Deluxe)',
+    artwork:"https://i.scdn.co/image/ab67616d0000b27386a8ab515de4b7aef28cd631",
+    url: './assets/audio/five.mp3',
+  },
+];
+
+export default playListData
